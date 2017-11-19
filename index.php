ca58e4ca3a72b3724ae8556938420f629e2c71b1
Bizkaitarra <Bizkaitarra@gmail.com>
2017-11-19 15:57:26 +0100

    Plugin first versión

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Plugin Name: Prueba con Composer y WP
+* Plugin URI: https://github.com/Bizkaitarra/kaixowp
+* Description: This plugin does some stuff with WordPress
+* Version: 1.0.0
+* Author: Jon González
+* Author URI: https://www.tudesarrolladorweb.com
+* License: GPL2
+*/
+
+echo "Hola desde la versión 1.0";
+
+?>
